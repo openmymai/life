a8be8290677980483c32084ac01122805f53291e
openmymai <maicmi@me.com>
2024-08-25 16:15:26 +0700

    Remove author from title

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -24,17 +24,17 @@ export function PostHeader({ title, coverImage, date, author }: Props) {
           src={coverImage}
         />
       </div>
-      <div className='max-w-2xl mx-auto'>
+      {/* <div className='max-w-2xl mx-auto'>
         <div className='block md:hidden mb-6'>
           <Avatar
             name={author.name}
             picture={author.picture}
           />
         </div>
-        {/* <div className='mb-6 text-lg'>
+        <div className='mb-6 text-lg'>
           <DateFormatter dateString={date} />
-        </div> */}
-      </div>
+        </div>
+      </div> */}
     </>
   );
 }
